ef19a67409d486da2477e33f367b614d235a0344
Rodney Lab <codesign@rodneylab.com>
2022-10-08 08:22:56 +0100

    fix: 💫 update redirect routes

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -5,23 +5,21 @@ export const load: LayoutServerLoad = async function load({ url }) {
 	try {
 		const { pathname } = url;
 		const imagesFiles = import.meta.glob('../assets/*.jpg');
-		const slugs = await Promise.all(
-			Object.keys(imagesFiles).map(async (path) => {
-				const src = path.split('/').at(-1);
-				if (!src) throw error(404, 'Not found');
+		const path = Object.keys(imagesFiles)[0];
+		const src = path.split('/').at(-1);
+		if (!src) throw error(404, 'Not found');
 
-				const [slug] = src?.split('.') ?? '';
-				return `/${slug}`;
-			})
-		);
-
-		if (!slugs.includes(pathname)) throw redirect(301, slugs[0]);
+		const [slug] = src?.split('.') ?? '';
+		if (pathname === '/') throw redirect(301, slug);
 		return {};
 	} catch (err: unknown) {
+		// rethrow a redirect error
 		const redirectError = err as { status: number; location: string };
 		if (redirectError.status === 301) {
 			throw redirect(301, redirectError.location);
 		}
+
+		// process any other type of error
 		console.error(err as string);
 		throw error(500, err as string);
 	}
